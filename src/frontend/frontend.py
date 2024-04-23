49b18d1300e90a736a0b830780cb28eb58680b39
landwarrior <tech_land_equilibrium@outlook.jp>
2024-04-24 00:40:05 +0900

    css や js にタイムスタンプを付与

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+
+def static_file_with_mtime(filename):
+    file_path = os.path.join(app.root_path, "static", filename)
+    try:
+        mtime = os.path.getmtime(file_path)
+    except OSError:
+        return url_for("static", filename=filename)
+    mtime_str = f"?v={mtime}"
+    full_path = url_for("static", filename=filename) + mtime_str
+    return full_path
+
+
+app.jinja_env.globals["url_for_with_mtime"] = static_file_with_mtime
 
 
 @app.route("/")
