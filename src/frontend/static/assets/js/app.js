2fb91e77ef08898740ae81681342700ea25648ad
landwarrior <tech_land_equilibrium@outlook.jp>
2024-05-05 02:49:12 +0900

    おそらく vue を使ってるせいでちらつく気がする
    ただし、 app.js を vue より先に記述しても変わらなかった。 defer のせいか？

--- src/frontend/static/assets/js/app.js
+++ src/frontend/static/assets/js/app.js
@@ -1,43 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    const theme = localStorage.getItem('theme') || systemTheme;
+    const selectedTheme = localStorage.getItem('theme') || 'auto';
+    let theme = selectedTheme;
+    if (selectedTheme === 'auto') {
+        theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
     document.documentElement.setAttribute('data-bs-theme', theme);
+    if (theme === 'light') {
+        document.getElementById('logo_dark').classList.toggle('d-inline-block');
+        document.getElementById('logo_dark').classList.toggle('d-none');
+    } else {
+        document.getElementById('logo_light').classList.toggle('d-inline-block');
+        document.getElementById('logo_light').classList.toggle('d-none');
+    }
+    toggleLogo(theme);
+    toggleIcon(selectedTheme);
+
+    document.getElementById('theme-switch').addEventListener('click', (e) => {
+        e.preventDefault();
+        let theme = e.target.dataset.theme;
+        localStorage.setItem('theme', theme);
+        if (theme === 'auto') {
+            theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+        }
+        document.documentElement.setAttribute('data-bs-theme', theme);
+        document.getElementById('current-theme').classList = e.target.querySelector('i').classList;
+        toggleLogo(theme);
+    });
+});
+
+/**
+ * テーマによってロゴを切り替える
+ * @param {string} theme テーマ
+ */
+function toggleLogo(theme) {
     if (theme === 'light') {
         document.getElementById('logo_dark').classList.add('d-none');
         document.getElementById('logo_dark').classList.remove('d-inline-block');
         document.getElementById('logo_light').classList.remove('d-none');
         document.getElementById('logo_light').classList.add('d-inline-block');
     } else {
-        document.getElementById('logo_light').classList.remove('d-inline-block');
         document.getElementById('logo_light').classList.add('d-none');
+        document.getElementById('logo_light').classList.remove('d-inline-block');
         document.getElementById('logo_dark').classList.remove('d-none');
         document.getElementById('logo_dark').classList.add('d-inline-block');
     }
+}
 
-    document.getElementById('theme-switch').addEventListener('click', (e) => {
-        e.preventDefault();
-        let theme = e.target.dataset.theme;
-        if (theme === 'auto') {
-            theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-        }
-        updateTheme(theme);
-        document.getElementById('current-theme').classList = e.target.querySelector('i').classList;
-        if (theme === 'light') {
-            document.getElementById('logo_dark').classList.add('d-none');
-            document.getElementById('logo_dark').classList.remove('d-inline-block');
-            document.getElementById('logo_light').classList.remove('d-none');
-            document.getElementById('logo_light').classList.add('d-inline-block');
-        } else {
-            document.getElementById('logo_light').classList.remove('d-inline-block');
-            document.getElementById('logo_light').classList.add('d-none');
-            document.getElementById('logo_dark').classList.remove('d-none');
-            document.getElementById('logo_dark').classList.add('d-inline-block');
-        }
-    });
-});
-
-// システムのカラースキームが変更されたときにテーマを更新する関数
-function updateTheme(selectedTheme) {
-    localStorage.setItem('theme', selectedTheme);
-    document.documentElement.setAttribute('data-bs-theme', selectedTheme);
+/**
+ * テーマによってアイコンを切り替える
+ * @param {string} theme テーマ
+ */
+function toggleIcon(theme) {
+    if (theme === 'auto') {
+        document.getElementById('current-theme').classList = ['bi bi-circle-half'];
+    } else if (theme === 'light') {
+        document.getElementById('current-theme').classList = ['bi bi-brightness-high-fill'];
+    } else {
+        document.getElementById('current-theme').classList = ['bi bi-moon-stars-fill'];
+    }
 }
