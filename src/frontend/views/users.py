84733e5a538de0d7173b8bd7c1225452d8a37ee9
landwarrior <tech_land_equilibrium@outlook.jp>
2024-05-06 00:59:41 +0900

    collapse でアイコンを変えたり、フッターの日付をセットしたり

--- src/frontend/views/users.py
+++ src/frontend/views/users.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, session
+from utils import get_common_data
 
 users_bp = Blueprint("users", __name__, url_prefix="/users")
 
 
 @users_bp.route("/list")
 def home():
+    common_data = get_common_data()
     session["hoge"] = "fuga"
-    return render_template("users/list.jinja")
+    return render_template("users/list.jinja", **common_data)
