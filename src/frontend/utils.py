c532b758a1fe1c01ebbf724e9dde5ce6116553c9
landwarrior <tech_land_equilibrium@outlook.jp>
2024-05-09 20:12:18 +0900

    あえて検索を遅くする

--- src/frontend/utils.py
+++ src/frontend/utils.py
@@ -1,6 +1,13 @@
 import datetime
 
 
-def get_common_data():
+def get_common_data() -> dict:
+    """汎用データ取得.
+
+    本来はここでログインユーザの情報を取得したり、メニューの出し分けを取得したりしたいかも.
+
+    Returns:
+        dict: 汎用データの辞書データ.
+    """
     common_data = {"now": {"year": datetime.datetime.now().year}}
     return common_data
--- src/frontend/views/users.py
+++ src/frontend/views/users.py
@@ -1,5 +1,7 @@
+import time
+
 import requests
-from flask import Blueprint, current_app, jsonify, render_template, request
+from flask import Blueprint, Response, current_app, jsonify, render_template, request
 from utils import get_common_data
 
 users_bp = Blueprint("users", __name__, url_prefix="/users")
@@ -12,8 +14,15 @@ def home():
 
 
 @users_bp.route("/api/list", methods=["POST"])
-def api_list():
+def api_list() -> Response:
+    """ユーザの一覧を json 形式で返す.
+
+    Returns:
+        Response: Flask の Response オブジェクト
+    """
     json_data = request.get_json(silent=True)
+    # 早すぎて動作確認できないので sleep を入れる
+    time.sleep(0.5)
     data = requests.get(current_app.config["BACKEND_URL"] + "/users", params=json_data).json()
 
     return jsonify(data)
